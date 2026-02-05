0e6b8832253423a073330878ee03900a1c10584d
Karlis Lauva <k@karlis.me>
2026-02-06 00:05:24 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Scoundrel
+
+Implementation of Scoundrel ruleset ([PDF](http://www.stfj.net/art/2011/Scoundrel.pdf)) in Rust.
+
+`cargo build` and play away!
+
+---
+2026 - ∞ [skazhy](https://karlis.me)
---
+++ src/main.rs
@@ -0,0 +1,186 @@
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+use std::cmp;
+use std::io;
+
+#[derive(Debug, Clone, Copy)]
+enum Suit {
+    Monster,
+    Weapon,
+    Potion,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Card {
+    value: i8,
+    suit: Suit,
+}
+
+impl Card {
+    fn formatted(self) -> String {
+        match self.suit {
+            Suit::Monster => format!("Monster {}", self.value),
+            Suit::Weapon => format!("Weapon {}", self.value),
+            Suit::Potion => format!("Potion: {}", self.value),
+        }
+    }
+}
+
+fn read_integer(options: &[String]) -> usize {
+    let max_val = options.len();
+    for (idx, o) in options.iter().enumerate() {
+        println!("[{idx}] - {o}");
+    }
+
+    loop {
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        match input.trim().parse::<usize>() {
+            Ok(n) if n < max_val => return n,
+            _ => println!("Invalid input, please enter a valid integer!"),
+        }
+    }
+}
+
+fn main() {
+    let mut cards: Vec<Card> = Vec::with_capacity(44);
+    let mut health = 20;
+    let mut weapon = 0;
+    let mut weapon_bound = 14;
+    let fight_opts = vec![
+        String::from("Fight barehanded"),
+        String::from("Fight with a weapon"),
+    ];
+
+    for i in 2..=10 {
+        cards.push(Card {
+            value: i,
+            suit: Suit::Monster,
+        });
+        cards.push(Card {
+            value: i,
+            suit: Suit::Monster,
+        });
+        cards.push(Card {
+            value: i,
+            suit: Suit::Weapon,
+        });
+        cards.push(Card {
+            value: i,
+            suit: Suit::Potion,
+        });
+    }
+
+    for i in 11..=14 {
+        cards.push(Card {
+            value: i,
+            suit: Suit::Monster,
+        });
+        cards.push(Card {
+            value: i,
+            suit: Suit::Monster,
+        });
+    }
+
+    let mut can_avoid_room = true;
+
+    let mut rng = thread_rng();
+    cards.shuffle(&mut rng);
+
+    // Initial room
+    let mut room_size: usize = 4;
+    let mut room: Vec<Card> = cards.drain(..room_size).collect();
+
+    loop {
+        println!("=== Remaining cards: {}", cards.len());
+
+        loop {
+            print!("=== Health: {health}");
+            if weapon > 0 {
+                print!(" | Weapon: {weapon}");
+                if weapon_bound < 14 {
+                    print!(" (max use: {weapon_bound})");
+                }
+            }
+            println!();
+
+            let mut inputs: Vec<String> = Vec::new();
+            if can_avoid_room && room.len() == 4 {
+                inputs.push(String::from("Avoid this room"));
+            }
+
+            for r in &room {
+                inputs.push(r.formatted());
+            }
+            let mut input = read_integer(&inputs);
+
+            if input == 0 && can_avoid_room && room.len() == 4 {
+                break;
+            } else {
+                if can_avoid_room && room.len() == 4 {
+                    input -= 1;
+                }
+
+                match room[input].suit {
+                    Suit::Potion => {
+                        health = cmp::min(20, health + room[input].value);
+                    }
+                    Suit::Weapon => {
+                        weapon = room[input].value;
+                        weapon_bound = 14;
+                    }
+                    Suit::Monster => {
+                        if weapon_bound >= room[input].value && weapon > 0 {
+                            let fight_opt = read_integer(&fight_opts);
+                            // Barehanded
+                            if fight_opt == 0 {
+                                health = cmp::max(0, health - room[input].value);
+                            // With a weapon
+                            } else {
+                                health = (health + weapon - room[input].value).clamp(0, health);
+                                weapon_bound = room[input].value;
+                            }
+                        } else {
+                            // Barehanded
+                            health = cmp::max(0, health - room[input].value);
+                        }
+                    }
+                }
+                room.remove(input);
+            }
+
+            if room.len() == 1 && cards.len() > 3 || room.is_empty() || health == 0 {
+                break;
+            }
+        }
+
+        room_size = cmp::min(4, cards.len());
+
+        // Last room avoided
+        if room.len() == 4 {
+            can_avoid_room = false;
+            room.shuffle(&mut rng);
+            cards.append(&mut room);
+            room = cards.drain(..room_size).collect();
+            continue;
+        }
+
+        // Death :(
+        if health == 0 {
+            println!("You died.");
+            break;
+        }
+        // Win
+        if cards.is_empty() {
+            println!("You win!");
+            break;
+        }
+
+        room.append(&mut cards.drain(..cmp::min(3, room_size)).collect());
+        can_avoid_room = cards.len() >= 4;
+    }
+}
